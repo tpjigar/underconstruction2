7dbe56198b029629d51add8d367b052b12d94917
jigarpatel <jigar.patel@planleft.com>
2022-12-05 23:55:41 +0530

    setup under construction

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'under-construction'], function () {
+    Route::get('/live-site', function() {
+        echo 'content!';
+    });
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
+
